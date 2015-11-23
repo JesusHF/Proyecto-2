88d1a71fdc1d4213b6702cb8b3fd9e2a975fb19c
JesusHF <jehernandez1996@gmail.com>
2015-11-23 11:39:12 +0100

    cambio leve

---
+++ Speedy Granny/Assets/Scripts/SeguirPJ.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeguirPJ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public Transform personaje;
+	public float separacion = 3f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=new Vector3(personaje.position.x+separacion, transform.position.y, transform.position.z);
+	}
+}
